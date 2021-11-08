1989b3f02bcb238ce915b9421a57ba5e3e02ed7d
weaponsforge <forgeweapons@gmail.com>
2021-11-08 20:07:47 +0800

    Rename references.

--- public/chars4.js
+++ public/chars4.js
@@ -3,14 +3,14 @@ if (document.images) {
 	tarrent_off.src= 'tarrent-off.jpg' ; 
 
 	tarrent_ovr= new Image(108,66); 
-	tarrent_ovr.src= 'tarrent.GIF'; 
+	tarrent_ovr.src= 'tarrent.gif';
 
 
         reiz_off= new Image(118,82); 
 	reiz_off.src= 'reiz-off.jpg' ; 
 
 	reiz_ovr= new Image(108,66); 
-	reiz_ovr.src= 'reiz.GIF' ; 
+	reiz_ovr.src= 'reiz.gif';
 
 
 
@@ -18,7 +18,7 @@ if (document.images) {
 	hellios_off.src= 'hellios-off.jpg' ; 
 
 	hellios_ovr= new Image(108,66); 
-	hellios_ovr.src= 'hellios.GIF' ; 
+	hellios_ovr.src= 'hellios.gif';
 
 
  
@@ -26,7 +26,7 @@ if (document.images) {
 	avatarr_off.src= 'avatarr-off.jpg' ; 
 
 	avatarr_ovr= new Image(108,66); 
-	avatarr_ovr.src= 'avatarr.GIF' ; 
+	avatarr_ovr.src= 'avatarr.gif';
 
 
 
@@ -34,7 +34,7 @@ if (document.images) {
 	tarrant_off.src= 'tarrant-off.jpg' ; 
 
 	tarrant_ovr= new Image(108,66); 
-	tarrant_ovr.src= 'tarrant.GIF' ; 
+	tarrant_ovr.src= 'tarrant.gif';
 
 	
 
